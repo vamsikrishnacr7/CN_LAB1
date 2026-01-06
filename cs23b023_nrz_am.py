2ba3280a4bc90078710f551dfb0f87f0fae29403
vamsikrishnacr7 <gvamsikrishna71817@gmail.com>
2026-01-06 15:25:29 +0530

    Amplitude modulation for the NRZ wave using the carrier wave

--- cs23b023_nrz_am.py
+++ cs23b023_nrz_am.py
@@ -1,6 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+
+# PART 1
+
 seq = np.array([1, 0, 1, 0, 1, 1, 0])
 
 
@@ -22,6 +25,7 @@ NRZ = nrz(seq, 200)
 print(len(NRZ))
 print(f"The NRZ encoding for the sequence {seq} is: \n {NRZ}")
 
+# PART 2
 
 def create_carrier(freq, duration, sampling_rate, amplitude = 1, phase = 0):
     t = np.linspace(0, duration, int(sampling_rate * duration), endpoint = False)
@@ -30,13 +34,12 @@ def create_carrier(freq, duration, sampling_rate, amplitude = 1, phase = 0):
     return t, wave
 
 
+freq = 20.0  
+s_rate = 1000.0 
+dur = len(NRZ) /s_rate   
 
-freq = 20.0   
-dur = 1.0    
-s_rate = 1000.0
 
 time, sine_wave = create_carrier(freq=freq, duration=dur, sampling_rate=s_rate)
-
 
 
 plt.figure(figsize=(10, 4))
@@ -46,6 +49,36 @@ plt.xlabel('Time (seconds)')
 plt.ylabel('Amplitude')
 plt.grid(True)
 plt.savefig("carrier_wave")
+
+
+# PART 3
+
+def amplitude_modulation(message, carrier):
+    return message * carrier
+
+
+amp_mod_wave = amplitude_modulation(NRZ, sine_wave)
+
+plt.figure(figsize = (10, 12))
+
+plt.subplot(3, 1, 1)
+plt.title("Message wave")
+plt.grid(True)
+plt.plot(NRZ)
+
+plt.subplot(3, 1, 2)
+plt.title("Carrier wave")
+plt.grid(True)
+plt.plot(sine_wave)
+
+plt.subplot(3, 1, 3)
+plt.title("Amplitude Modulated wave")
+plt.grid(True)
+plt.plot(amp_mod_wave)
+
+plt.savefig("amplots.pdf")
+
+
 
 
 
